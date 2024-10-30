4e5facab6cb5f6351d91807c93d6cca8c18de874
talhabinalam <mdtalhabinalam@outlook.com>
2024-10-31 01:48:17 +0600

    first commit

---
+++ dashboard/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('staff/', staff, name='staff'),
+    path('product/', product, name='product'),
+    path('order/', order, name='order'),
+]
